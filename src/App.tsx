5e680a974a564551c9a52c7f60705224743585f1
ConstantineMikhalev <mikhalevk@gmail.com>
2024-06-07 14:03:08 +0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
+import { Welcome } from "./components/Welcome";
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -34,6 +35,7 @@ function App() {
       <AppContainer>
         <FlexBoxCol>
           <FlexBoxRow>
+            <Welcome/>
             <TonConnectButton />
             <Button>
               {network
@@ -43,9 +45,9 @@ function App() {
                 : "N/A"}
             </Button>
           </FlexBoxRow>
-          <Counter />
+          {/* <Counter /> */}
           <TransferTon />
-          <Jetton />
+          {/* <Jetton /> */}
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>
