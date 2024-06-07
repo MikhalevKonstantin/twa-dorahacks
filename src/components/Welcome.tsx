b91b5ea31618f252c2aafa0dda227959fff0710c
ConstantineMikhalev <mikhalevk@gmail.com>
2024-06-07 13:52:12 +0400

    tonchi2

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Address, toNano, comment } from "ton";
+import { useTonConnect } from "../hooks/useTonConnect";
+import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from "./styled/styled";
+
+export function Welcome() {
+  const { sender, connected } = useTonConnect();
+
+  const [tonAmount, setTonAmount] = useState("0.01");
+  const [text, setText] = useState("");
+  const [tonRecipient, setTonRecipient] = useState(
+    "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
+  );
+
+  return (
+    <Card>
+      <FlexBoxCol>
+        <h3>Welcome to TONCHI</h3>
+        <FlexBoxRow>
+          <label>Here you can connect your wallet </label>
+        </FlexBoxRow>
+        <FlexBoxRow>
+          <label>And send some TONes </label>
+        </FlexBoxRow>
+      </FlexBoxCol>
+    </Card>
+  );
+}
